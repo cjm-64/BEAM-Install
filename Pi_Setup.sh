e67c83fb4f412285e6dddfd6fe086667718f7f96
Chris <Christopher.Morris7164@gmail.com>
2023-02-23 09:25:17 -0500

    Updating to OpenCV 4.6.1

--- Pi_Setup.sh
+++ Pi_Setup.sh
@@ -16,7 +16,6 @@ function install_dependency {
 echo "Installing Dependencies"
 
 
-install_dependency libopencv-dev
 install_dependency g++
 install_dependency build-essential
 install_dependency checkinstall
@@ -25,6 +24,8 @@ install_dependency pkg-config
 install_dependency yasm
 install_dependency libtiff5-dev
 install_dependency libjpeg-dev
+install_dependency libjpeg8-dev
+install_dependency libpng-dev
 install_dependency libavcodec-dev
 install_dependency libavformat-dev
 install_dependency libswscale-dev
@@ -32,16 +33,8 @@ install_dependency libdc1394-dev
 install_dependency libxine2-dev
 install_dependency libv4l-dev
 install_dependency libprotobuf-dev
-
-##Python installs are standard on RPi Bullseye
-##install_dependency python-dev
-##install_dependency python-numpy
-
-
+install_dependency python3-dev
 install_dependency libtbb-dev
-install_dependency libqt5x11extras5
-install_dependency libqt5opengl5
-install_dependency libqt5opengl5-dev
 install_dependency libgtk2.0-dev
 install_dependency libmp3lame-dev
 install_dependency libopencore-amrnb-dev
@@ -58,6 +51,9 @@ install_dependency libglu1-mesa-dev
 #IDE Installs
 install_dependency qtcreator
 install_dependency qt5-assistant
+install_dependency libqt5opengl5
+install_dependency libqt5opengl5-dev
+install_dependency libqt5x11extras5
 
 #Misc Installs
 install_dependency matchbox-keyboard
@@ -69,12 +65,12 @@ install_dependency default-jdk
 mkdir OpenCV
 cd OpenCV
 
-Version="4.5.1.zip"
+Version="4.6.0"
 
-echo "Downloading OpenCV 4.0.0"
-wget https://github.com/opencv/opencv/archive/$Version
+echo "Downloading OpenCV 4.6.0"
+wget https://github.com/opencv/opencv/archive/$Version.zip
 
-echo "Unzipping OpenCV 4.0.0"
+echo "Unzipping OpenCV 4.6.0"
 unzip $Version
 
 echo "Making build directory"
@@ -83,10 +79,10 @@ cd build
 
 
 echo "Setting build options"
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_GENERATE_PKGCONFIG=ON ../opencv-4.5.1
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_GENERATE_PKGCONFIG=ON ../opencv-$Version
 
 
-## -D WITH_PROTOBUF=ON -D WITH_WEBP=OFF
+## -D CMAKE_DISABLE_FIND_PACKAGE_TBB=ON -D WITH_PROTOBUF=OFF-D WITH_WEBP=OFF
 ## -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.4.3/modules
 
 make clean
@@ -94,7 +90,7 @@ make clean
 echo "Building"
 make -j4
 
-echo "Installing OpenCV 4.0.0"
+echo "Installing OpenCV 4.6.0"
 sudo make install
 
 echo "Moving files to proper location"
@@ -103,4 +99,5 @@ cd && cd .. && cd .. && cd /usr/local/include/opencv4 && sudo cp -avr /usr/local
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 
-echo "OpenCV" pkg-config --modversion opencv4 "is ready to be used"
+VerNum=$(pkg-config --modversion opencv4)
+echo "OpenCV" $VerNum "is ready to be used"
