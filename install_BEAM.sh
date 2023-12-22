5a6b30f66c58f527041209b3c35a10f1fea12cc3
Chris <61848401+cjm-64@users.noreply.github.com>
2023-12-22 14:26:37 -0500

    Update install_BEAM.sh to make OpenKeyboard an executable

--- install_BEAM.sh
+++ install_BEAM.sh
@@ -2,7 +2,7 @@
 
 currentdir=$(pwd)
 
-git clone https://github.com/cjm-64/BEAM && cd BEAM && git switch fps && qmake BEAM.pro && make && ls -la
+cd .. && git clone https://github.com/cjm-64/BEAM && cd BEAM && git switch fps && qmake BEAM.pro && make && chmod +x OpenKeyboard.sh && ls -la
 
 cd 
 cd $currentdir
